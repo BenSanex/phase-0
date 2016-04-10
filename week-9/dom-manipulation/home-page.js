85834572093477c67d24d14cfdee5b15ca5141bd
BenSanex <ben.sanecki@gmail.com>
2016-04-10 17:50:58 -0500

    Complete dom manipulation challenge with reflection

---
+++ week-9/dom-manipulation/home-page.js
@@ -0,0 +1,33 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+document.getElementById('release-0').className = 'done';
+
+
+// Release 1:
+document.getElementById('release-1').style.display = 'none';
+
+
+// Release 2:
+document.getElementsByTagName('H1')[0].innerHTML = 'I completed release 2';
+
+
+// Release 3:
+document.getElementById('release-3').style.background = "#955259";
+
+
+// Release 4:
+for (var i=0; i<document.getElementsByClassName('release-4').length; i ++) {
+	document.getElementsByClassName('release-4')[i].style.fontSize = '2em';
+};
+
+
+// Release 5:
+var tmpl = document.getElementById('hidden');
+document.body.appendChild(tmpl.content.cloneNode(true));
